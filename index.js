d005d298c711073f4837bc7efeea2090179a1c22
ScottDenton <scottyd_14@hotmail.com>
2018-09-12 15:02:00 -0700

    questions sorted

--- index.js
+++ index.js
@@ -26,42 +26,35 @@ function dog (breed, build, activityLevel, inside, hair) {
 var dogBreeds = [labrador, frenchBulldog, germanShepherd, pug, italianGreyhound, maltese, bearededCollie];
 
 // list out questions and answers
-var question1 = "Would you prefer a small or a large dog ?";
-var answer1_1 = "Small";
-var answer1_2 = "Medium";
-var answer1_3 = "Large";
+var question1 = "What size dog would you prefer ?";
+var answerQ1 = ["Small", "Medium", "Large"];
+
 
 var question2 = "How active would you like your new dog to be ?";
-var answer2_1 = "Not Very";
-var answer2_2 = "Moderate";
-var answer2_3 = "Very Active";
+var answerQ2 = ["Not Very", "Moderate", "Very Active"];
+
 
 var question3 = "Would you like an inside or an outside dog ?";
-var answer3_1 = "Inside";
-var answer3_2 = "Both";
-var answer3_3 = "Outside";
+var answerQ3 = ["Inside", "Either", "Outside"];
+
 
 var question4 = "Do you prefer long hair or short haired dogs";
-var answer4_1 = "Short";
-var answer4_2 = "Medium";
-var answer4_3 = "Long";
+var answerQ4 = ["Short", "Medium", "Long"];
 
 var allQuestions = [question1, question2, question3, question4];
+var allAnswers = [answerQ1, answerQ2, answerQ3, answerQ4]
+
 
 
 // insert question into UI
 // ********************************* section im working on
-for(var i = 0; i < allQuestions.length; i++) {
-
-document.getElementById("question-text").textContent = allQuestions[i];
-console.log(allQuestions[i]);
-
-}
+var question = 0;
+document.getElementById("question-text").textContent = allQuestions[question];
 
 // insert answer options into UI
-document.getElementById("label1").textContent = answer1_1;
-document.getElementById('label2').textContent = answer1_2;
-document.getElementById('label3').textContent = answer1_3;
+document.getElementById("label1").textContent = allAnswers[question][0];
+document.getElementById('label2').textContent = allAnswers[question][1];
+document.getElementById('label3').textContent = allAnswers[question][2];
 
 
 //when  question is answered remove dogs from array that dont suit answer given
